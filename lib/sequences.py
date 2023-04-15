0f9bb8915f1fe2e6fe9cc4360b9b51925ded76c3
Brendan <brendan.b517@gmail.com>
2023-04-15 11:50:13 -0400

    Passed all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    index = 2
+    if length == 0:
+        fibonacci.clear()
+    elif length == 1:
+        fibonacci.pop(1)
+    else:
+        while index < length:
+            next_value = fibonacci[index - 2] + fibonacci[index - 1]
+            fibonacci.append(next_value)
+            index += 1
+    print(fibonacci)
+    return fibonacci
